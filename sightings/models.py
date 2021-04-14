de57a1774a01dcce3215278945aee3ff730711dc
Haocheng Tian <ht2544@columbia.edu>
2021-04-14 17:41:07 +0000

    Update form/urls/models/views

--- sightings/models.py
+++ sightings/models.py
@@ -1,159 +1,30 @@
 from django.db import models
-from django.utils.translation import gettext as _
 
-# Create your models here.
-class Sight(models.Model):
-    Longitude  = models.FloatField(
-            help_text = _('Longitude of the sight'),)
+class Squirrel(models.Model):
 
-    Latitude = models.FloatField(
-            help_text = _('Latitude of the sight'),)
+    Latitude = models.FloatField(blank=False,) 
+    Longitude = models.FloatField(blank=False,)
+    Unique_Squirrel_ID = models.CharField(max_length=1000,blank=False, primary_key=True,)
+    Shift = models.CharField(max_length=1000, null=True,blank=False,)
+    Date = models.DateField(null=True,blank=False,)
+    Age = models.CharField(max_length=1000, null=True,)
+    Primary_Fur_Color = models.CharField(max_length=1000, null=True,blank=True,)
+    Location = models.CharField(max_length=1000, null=True,blank=True,)
+    Specific_Location = models.CharField(max_length=1000, null=True,blank=True,)
+    Running = models.BooleanField(blank=True,)
+    Chasing = models.BooleanField(blank=True,)
+    Climbing = models.BooleanField(blank=True,)
+    Eating = models.BooleanField(blank=True,)
+    Foraging = models.BooleanField(blank=True,)
+    Other_Activities = models.CharField(max_length=1000, null=True, blank=True,)
+    Kuks = models.BooleanField(blank=True,)
+    Quaas = models.BooleanField(blank=True,)
+    Moans = models.BooleanField(blank=True,)
+    Tail_Flags = models.BooleanField(blank=True,)
+    Tail_Twitches = models.BooleanField(blank=True,)
+    Approaches = models.BooleanField(blank=True,)
+    Indifferent = models.BooleanField(blank=True,)
+    Runs_From = models.BooleanField(blank=True,)
 
-    Unique_Squirrel_Id = models.CharField(
-            help_text = _('The unique ID of the squirrel'),
-            max_length = 25,
-            )
-    AM = 'AM'
-    PM = 'PM'
-    SESSION=(
-            (AM,'AM'),
-            (PM,'PM'),
-            )
-
-    Shift = models.CharField(
-            help_text = _('Whether the sight is in the morning or late afternoon?'),
-            max_length=16,
-            choices=SESSION,
-            blank=True)
-
-    Date = models.DateField(
-            help_text = _('The format is in yyyy-mm-dd'),
-            null = True,
-            blank=True)
-
-    Adult = 'Adult'
-    Juvenile = 'Juvenile'
-    Unknown = '?'
-    AGE_CHOICE=(
-            (Adult,'Adult'),
-            (Juvenile, 'Juvenile'),
-            (None, ''),
-            (Unknown, '?'),
-            )
-
-    Age = models.CharField(
-            help_text = _('Age of the squirrel'),
-            max_length=16,
-            choices = AGE_CHOICE,
-            blank = True
-            )
-
-    Black = 'Black'
-    Gray = 'Gray'
-    Cinnamon = 'Cinnamon'
-    COLOR_CHOICE=(
-            (Black, 'Black'),
-            (Gray, 'Gray'),
-            (Cinnamon, 'Cinnamon'),
-            (None, ''),
-            )
-
-    Primary_Fur_Color = models.CharField(
-            help_text = _('Fur color'),
-            max_length=16,
-            choices = COLOR_CHOICE,
-            blank = True
-            )
-
-    Ground_Plane = 'Ground Plane'
-    Above_Ground = 'Above Ground'
-    LOCATION_CHOICE=(
-            (Ground_Plane, 'Ground Plane'),
-            (Above_Ground, 'Above Ground'),
-            (None, ''),
-            )
-    
-    Location =  models.CharField(
-            help_text = _('Location'),
-            max_length=128,
-            choices = LOCATION_CHOICE,
-            blank = True
-            )
-
-    Specific_Location = models.CharField(
-            help_text = _('Additional notes to the location'),
-            max_length=128,
-            blank = True,
-            )
-
-    Running = models.NullBooleanField(
-            help_text = _('Running'),
-            blank=True,
-    )
-    
-    Chasing = models.NullBooleanField(
-            help_text = _('Chasing'),
-            blank=True,
-    )
-
-    Climbing = models.NullBooleanField(
-            help_text = _('Climbing'),
-            blank=True,
-    )
-
-    Eating = models.NullBooleanField(
-            help_text = _('Eating'),
-            blank=True,
-    )
-
-    Foraging = models.NullBooleanField(
-            help_text = _('Foraging'),
-            blank=True,
-    )
-
-    Other_Activities = models.CharField(
-        help_text = _('Other Activities'),
-        max_length = 128,
-        null = True,
-        blank = True
-    )
-
-    Kuks = models.NullBooleanField(
-            help_text = _('Kuks'),
-            blank=True,
-    )
-
-    Quaas = models.NullBooleanField(
-            help_text = _('Quaas'),
-            blank=True,
-    )
-
-    Moans = models.NullBooleanField(
-            help_text = _('Moans'),
-            blank=True,
-    )
-
-    Tail_Flags = models.NullBooleanField(
-            help_text = _('Tail_Flags'),
-            blank=True,
-    )
-
-    Tail_Twitches = models.NullBooleanField(
-            help_text = _('Tail_Twitches'),
-            blank=True,
-    )
-
-    Approaches = models.NullBooleanField(
-            help_text = _('Approaches'),
-            blank=True,
-    )
-
-    Indifferent = models.NullBooleanField(
-            help_text = _('Indifferent'),
-            blank=True,
-    )
-    
-    Runs_From = models.NullBooleanField(
-            help_text = _('Runs_From'),
-            blank=True,
-    )
+    def __str__(self):
+        return self.Unique_Squirrel_ID
--- sightings/views.py
+++ sightings/views.py
@@ -1,117 +1,84 @@
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
-from .models import Sight
-from django.shortcuts import redirect
-from .forms import SightForm
+from sightings.models import Squirrel
+from sightings.forms import AddNewForm,UpdateForm
 
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
-def homepage_view(request):
-    return render(request,'sightings/homepage.html')
+def index(request):
+    squirrels = Squirrel.objects.all()
 
-def map_view(request):
-    sights = Sight.objects.all()[:100]
     context = {
-            'sights':sights,
+            'squirrels': squirrels,
             }
-    return render(request, 'sightings/map.html', context)
+    field_list = [field.name for field in Squirrel._meta.get_fields()]
+    return render(request, "sightings/index.html", context)
 
+def squirrel_details(request, Unique_Squirrel_ID):
+    squirrel = get_object_or_404(Squirrel, pk=Unique_Squirrel_ID)
 
-def list_sights(request):
-    sights = Sight.objects.all()
-    fields = ['Unique_Squirrel_Id','Longtitude','Latitude','Date','Shift']
     context = {
-            'sights':sights,
-            'fields':fields,
-            }
-    return render(request, 'sightings/list.html', context)
-
-
-def update_sights(request,Unique_Squirrel_Id):
-    sight = Sight.objects.get(Unique_Squirrel_Id=Unique_Squirrel_Id)
+        'squirrel': squirrel,
+    }
     if request.method == 'POST':
-        form = SightForm(request.POST, instance = sight)
+        form = UpdateForm(request.POST, instance=squirrel)
         if form.is_valid():
             form.save()
-            return redirect(f'/sightings')
-    else:
-        form = SightForm(instance = sight)
+            return HttpResponse('updated')
+        else:
+            return JsonResponse({'errors': form.errors}, status=400)
 
-    context = {
-            'form':form,
-            }
-    return render(request, 'sightings/update.html', context)
+    return render(request, 'sightings/detail.html', context)
 
-
-def add_sights(request):
+def add(request):
     if request.method == 'POST':
-        form = SightForm(request.POST)
+        form = AddNewForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect(f'/sightings/')
-    else:
-        form = SightForm()
+            return HttpResponse('You have added a new squirrel data.')
+        else:
+            return JsonResponse({'errors': form.errors}, status=400)
+    return render(request, 'sightings/add.html', {})
 
+def stats(request):
+    squirrels = Squirrel.objects.all()
+    count = 0
+    latitude = 0
+    longitude = 0
+    adult = 0
+    juvenile = 0
+    run_true = 0
+    run_false = 0
+    for squirrel in squirrels:
+        count += 1
+        latitude1 = getattr(squirrel, 'Latitude')
+        latitude += latitude1
+        longitude1 = getattr(squirrel, 'Longitude')
+        longitude += longitude1
+        age1 = getattr(squirrel, 'Age')
+        if age1 == 'Adult':
+            adult += 1
+        elif age1 == 'Juvenile':
+            juvenile += 1
+        else:
+            pass
+        running1 = getattr(squirrel, 'Running')
+        if running1 == True:
+            run_true += 1
+        elif running1 == False:
+            run_false += 1
+        else:
+            pass
+    
+    latitude_avg = latitude/count
+    longitude_avg = longitude/count
     context = {
-            'form':form,
+            'count': count,
+            'latitude_avg': latitude_avg,
+            'longitude_avg': longitude_avg,
+            'adult': adult,
+            'juvenile': juvenile,
+            'run_true': run_true,
+            'run_false': run_false,
             }
-
-    return render(request, 'sightings/add.html', context)
-
-
-def stats_view(request):
-
-    sights = Sight.objects.all()
-
-    # shift
-    AM_n = sights.filter(Shift='AM').count()
-    PM_n = sights.filter(Shift='PM').count()
-    AM_pct = AM_n/(AM_n + PM_n)
-    AM_pct = "{:.2%}".format(AM_pct)
-    PM_pct = PM_n/(AM_n + PM_n)
-    PM_pct = "{:.2%}".format(PM_pct)
-    # age
-    Juvenile_n = sights.filter(Age='Juvenile').count()
-    Adult_n = sights.filter(Age='Adult').count()
-    Juvenile_pct = Juvenile_n / (Juvenile_n + Adult_n)
-    Juvenile_pct = "{:.2%}".format(Juvenile_pct)
-    Adult_pct = Adult_n / (Juvenile_n + Adult_n)
-    Adult_pct = "{:.2%}".format(Adult_pct)
-    # Primary_Fur_Color
-    Black_n = sights.filter(Primary_Fur_Color='Black').count()
-    Gray_n = sights.filter(Primary_Fur_Color='Gray').count()
-    Cinnamon_n = sights.filter(Primary_Fur_Color='Cinnamon').count()
-    Black_pct = Black_n / (Black_n+Gray_n+Cinnamon_n)
-    Black_pct = "{:.2%}".format(Black_pct)
-    Gray_pct = Gray_n / (Black_n+Gray_n+Cinnamon_n)
-    Gray_pct = "{:.2%}".format(Gray_pct)
-    Cinnamon_pct = Cinnamon_n / (Black_n+Gray_n+Cinnamon_n)
-    Cinnamon_pct = "{:.2%}".format(Cinnamon_pct)
-    # Location
-    Above_Ground_n = sights.filter(Location='Above Ground').count()
-    Ground_Plane_n = sights.filter(Location='Ground Plane').count()
-    Above_Ground_pct = Above_Ground_n / (Above_Ground_n+Ground_Plane_n)
-    Above_Ground_pct = "{:.2%}".format(Above_Ground_pct)
-    Ground_Plane_pct = Ground_Plane_n / (Above_Ground_n+Ground_Plane_n)
-    Ground_Plane_pct= "{:.2%}".format(Ground_Plane_pct)
-    # Runs_From
-    True_n = sights.filter(Runs_From=True).count()
-    False_n = sights.filter(Runs_From=False).count()
-    True_pct = True_n / (True_n+False_n)
-    True_pct = "{:.2%}".format(True_pct)
-    False_pct = False_n / (True_n+False_n)
-    False_pct = "{:.2%}".format(False_pct)
-
-    context = {
-            'Total':sights.count(),
-            'Shift': {'AM': AM_n,'PM': PM_n},
-            'Shift_pct': {'AM': AM_pct,'PM': PM_pct},
-            'Age': {'Juvenile': Juvenile_n, 'Adult': Adult_n},
-            'Age_pct': {'Juvenile': Juvenile_pct, 'Adult': Adult_pct},
-            'Primary_Fur_Color': {'Black':Black_n, 'Gray':Gray_n, 'Cinnamon':Cinnamon_n},
-            'Primary_Fur_Color_pct': {'Black':Black_pct, 'Gray':Gray_pct, 'Cinnamon':Cinnamon_pct},
-            'Location': {'Above_Ground':Above_Ground_n, 'Ground_Plane':Ground_Plane_n},
-            'Location_pct': {'Above_Ground':Above_Ground_pct, 'Ground_Plane':Ground_Plane_pct},
-            'Runs_From': {'True':True_n, 'False':False_n},
-            'Runs_From_pct': {'True':True_pct, 'False':False_pct},
-            }
-    return render(request, 'sightings/stats.html', {'context':context})
+    return render(request, "sightings/stats.html", context)
