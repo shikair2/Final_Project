a7bc988ddf64b3fcab6d7a505ee9782d0cb7d112
Haocheng Tian <ht2544@columbia.edu>
2021-04-14 17:49:16 +0000

    html/py files modified

--- SquirrelTrack/settings.py
+++ SquirrelTrack/settings.py
@@ -1,31 +1,31 @@
-import os
+from pathlib import Path
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_4_pdw$u4&-mhzov+!e9kkxpvf-vg(pue5$=3cnh-qyx1m3@s2'
+SECRET_KEY = 'ddd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'sightings.apps.SightingsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sightings',
+    'map',
 ]
 
 MIDDLEWARE = [
@@ -38,7 +38,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'squirrel.urls'
+ROOT_URLCONF = 'project.urls'
 
 TEMPLATES = [
     {
@@ -56,22 +56,22 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'squirrel.wsgi.application'
+WSGI_APPLICATION = 'project.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
 
 # Password validation
-# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
+# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -90,7 +90,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/3.0/topics/i18n/
+# https://docs.djangoproject.com/en/3.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -104,7 +104,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'collected-static'
--- sightings/management/commands/export_squirrel_data.py
+++ sightings/management/commands/export_squirrel_data.py
@@ -1,73 +1,26 @@
 import csv
-import datetime
+  
 from django.core.management.base import BaseCommand
-from sightings.models import Sight
-
-class Command(BaseCommand):
-    def add_arguments(self,parser):
-        parser.add_argument('path')
-
+from sightings.models import Squirrel
+                                                          
+class Command(BaseCommand):                                                         
+    help = ("Exporting data to .csv")
+                                                          
+    def add_arguments(self, parser):
+        parser.add_argument('csv_file', type=str)
+                                                          
     def handle(self, *args, **options):
-        with open(options['path'][0],'w') as fp:
-            squirrel_writer = csv.DictWriter(
-                    fp, 
-                    delimiter=',',
-                    fieldnames=[
-                        'X',
-                        'Y',
-                        'Unique Squirrel ID',
-                        'Shift',
-                        'Date',
-                        'Age',
-                        'Primary Fur Color',
-                        'Location',
-                        'Specific Location',
-                        'Running',
-                        'Chasing',
-                        'Climbing',
-                        'Eating',
-                        'Foraging',
-                        'Other Activities',
-                        'Kuks',
-                        'Quaas',
-                        'Moans',
-                        'Tail flags',
-                        'Tail twitches',
-                        'Approaches',
-                        'Indifferent',
-                        'Runs from'
-                        ]
-                    )
-            squirrel_writer.writeheader()
-    
-            for row in Sight.objects.all():
-                if row.Date: 
-                    date_form = row.Date.strftime('%m%d%Y')
-                else:
-                    date_form = row.Date
+        file_=options['csv_file']
 
-                squirrel_writer.writerow({
-                    'X':row.Longitude,
-                    'Y':row.Latitude,
-                    'Unique Squirrel ID':row.Unique_Squirrel_Id,
-                    'Shift':row.Shift,
-                    'Date':date_form,
-                    'Age':row.Age,
-                    'Primary Fur Color':row.Primary_Fur_Color,
-                    'Location':row.Location,
-                    'Specific Location':row.Specific_Location,
-                    'Running':row.Running,
-                    'Chasing':row.Chasing,
-                    'Climbing':row.Climbing,
-                    'Eating':row.Eating,
-                    'Foraging':row.Foraging,
-                    'Other Activities':row.Other_Activities,
-                    'Kuks':row.Kuks,
-                    'Quaas':row.Quaas,
-                    'Moans':row.Moans,
-                    'Tail flags':row.Tail_Flags,
-                    'Tail twitches':row.Tail_Twitches,
-                    'Approaches':row.Approaches,
-                    'Indifferent':row.Indifferent,
-                    'Runs from':row.Runs_From,          
-                    })
+        field_list = [f.name for f in Squirrel._meta.get_fields()]
+        attr_list = ['Latitude', 'Longitude', 'Unique_Squirrel_ID', 'Shift', 'Date', 'Age', 'Primary_Fur_Color', 'Location', 'Specific_Location', 'Running', 'Chasing', 'Climbing', 'Eating', 'Foraging', 'Other_Activities', 'Kuks', 'Quaas', 'Moans', 'Tail_Flags', 'Tail_Twitches', 'Approaches', 'Indifferent', 'Runs_From']
+        with open(file_, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(field_list)
+
+            squirrels = Squirrel.objects.all()
+                                                          
+            for obj in squirrels:
+                value_list = [getattr(obj, f) for f in attr_list]
+                writer.writerow(value_list)
+        csvfile.close()
